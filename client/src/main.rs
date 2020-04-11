035feba41d16735cfcbc91c7352637a662a4ec3b
Lukas Nagel <lukaswnagel@gmail.com>
2020-04-11 14:13:32 +0200

    first draft server
    Co-authored-by: maga-EB <markus.gasser@elektrobit.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Message {
+    Connect { user_name: String },
+    Quit {user_name: String},
 }
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+
+    let message = Message::Connect{user_name: String::from("Lukas")};
+    let mut serialized = serde_json::to_vec(&message).unwrap();
+    let _result = stream.write(&mut serialized);
+    
+    let message = Message::Quit{user_name: String::from("Lukas")};
+    let mut serialized = serde_json::to_vec(&message).unwrap();
+    let _result = stream.write(&mut serialized);
+
+    Ok(())
+    //test
+} // the stream is closed here
+
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+
+use serde::{Serialize, Deserialize};
+use std::net::TcpListener;
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Message {
+    Connect { user_name: String },
+    Quit {user_name: String},
+}
+
+fn main() -> std::io::Result<()> {
+    // let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:34254").unwrap();
+
+
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("new client!");
+                
+                // let mut buffer: Vec<u8> = Vec::new();
+            
+                // // stream.write(&[1])?;
+                // stream.read(&mut buffer)?;
+            
+                let mut de = serde_json::Deserializer::from_reader(stream);
+                
+                
+                loop {
+
+                    // let read_bytes = stream.peek(&mut [0, 1000]).unwrap();
+
+
+                    
+
+                    let result = Message::deserialize(&mut de);
+
+                    if let Ok(message) = result {
+                        println!("{:?}", message);
+
+                            match message {
+                                Message::Connect{user_name} => println!("user {} received!", user_name),
+                                _ => println!("something else was received!"),
+                            }
+                    } else {
+                        // println!("didn't get anything!");
+                    }
+
+                    // match result {
+                    //     Result::Message(message) => {
+                    //         println!("{:?}", message);
+
+                    //         match message {
+                    //             Message::Connect{user_name} => println!("user {} received!", user_name),
+                    //             _ => println!("something else was received!"),
+                    //         }
+                    //     },
+                    //     // serde_json::error::Error
+                    //     _ => {
+                    //         println!("didn't get anything!");
+                    //     }
+                    // }
+
+
+                }
+                
+
+            }
+            Err(_e) => { /* connection failed */ }
+        }
+    }
+
+    Ok(())
+
 }
