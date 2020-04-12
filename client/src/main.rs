53b37a0a6d83014de94a9ee589dec9490a76d493
Lukas Nagel <lukaswnagel@gmail.com>
2020-04-12 11:57:37 +0200

    activate sending

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,31 +6,31 @@ use std::env;
 mod testing;
 mod message;
 
-
+// use message;
 
 fn main() -> std::io::Result<()> {
-    // let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
 
-    // let message = Message::Connect{user_name: String::from("Lukas")};
-    // let mut serialized = serde_json::to_vec(&message).unwrap();
-    // let _result = stream.write(&mut serialized);
+    let mymessage = message::Message::Connect{user_name: String::from("Lukas")};
+    let mut serialized = serde_json::to_vec(&mymessage).unwrap();
+    let _result = stream.write(&mut serialized);
 
-    // let message = Message::Quit{user_name: String::from("Lukas")};
-    // let mut serialized = serde_json::to_vec(&message).unwrap();
-    // let _result = stream.write(&mut serialized);
+    let mymessage = message::Message::Quit{user_name: String::from("Lukas")};
+    let mut serialized = serde_json::to_vec(&mymessage).unwrap();
+    let _result = stream.write(&mut serialized);
 
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        panic!("usage: cmd [filename.yaml]");
-    }
+    // if args.len() != 2 {
+    //     panic!("usage: cmd [filename.yaml]");
+    // }
 
-    let user_actions = testing::read_user_actions(&args[1]).unwrap();
+    // let user_actions = testing::read_user_actions(&args[1]).unwrap();
 
-    for message_and_duration in user_actions.iter() {
-        println!("{:?}", message_and_duration.message);
-        println!("duration: {}s", message_and_duration.duration);
-    }
+    // for message_and_duration in user_actions.iter() {
+    //     println!("{:?}", message_and_duration.message);
+    //     println!("duration: {}s", message_and_duration.duration);
+    // }
 
 
     Ok(())
